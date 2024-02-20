8f76f36d0561db029b070caa2ed04bee57a791cc
admin <admin@admins-MacBook-Pro.local>
2024-02-20 10:21:34 +0000

    first commit

---
+++ src/main/java/com/cab/bookingService/CabBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cab.bookingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingServiceApplication.class, args);
+	}
+
+}
